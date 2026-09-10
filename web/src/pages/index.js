f0554442c21f978dee2018496aaa28e1a40d184d
mrcodexarck <mrcodexarck@gmail.com>
2026-09-10 12:00:17 -0500

    cambio de carpetas

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -8,7 +8,7 @@ const highlights = [
     icon: '📚',
     title: 'Cursos y talleres',
     text: 'Rutas de aprendizaje por módulos con contenido progresivo y ejercicios prácticos.',
-    url: '/Cursos/curso-contabilidad/3-modulo-tres',
+    url: '/Cursos/curso-contabilidad/modulo-tres',
   },
   {
     icon: '🧭',
